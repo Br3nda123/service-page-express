be2b65f0088adc7bea2659801d6cdd9afe195b20
Brandon Kunicki <brandon.kunicki@gmail.com>
2021-02-03 00:21:05 +0100

    admin login, admin news

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  db: 'mongodb+srv://admin:1QfJTwG7y3A3cNE5@cluster0.ic2wp.mongodb.net/<dbname>?retryWrites=true&w=majority',
+  keySession: ['TWOJKLUCZ'],
+  maxAgeSession: 24 * 60 * 60 * 1000,
+};
